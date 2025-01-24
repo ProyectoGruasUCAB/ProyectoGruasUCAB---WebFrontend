fca1dddbc978e1407af18b4a27ba5f6b13f5faff
Marco Agrusa <marcoagrusa@gmail.com>
2025-01-23 21:12:20 -0400

    FW-Ligeros Cambios Clients

--- gruasucab-webclient/src/components/Order/OrderListClient.js
+++ gruasucab-webclient/src/components/Order/OrderListClient.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { getOrdersByClientId } from '../../api/apiServiceOrder';
-import { setAuthToken } from '../../api/apiAuth'; // Asegúrate de tener esta función para configurar el token de autenticación
+import { setAuthToken } from '../../api/apiAuth';
+import { ListGroup, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const OrderListClient = ({ id }) => {
   const [orders, setOrders] = useState([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchOrders = async () => {
       try {
-        const token = localStorage.getItem('token');
-        setAuthToken(token);
-        const response = await getOrdersByClientId(id);
-        setOrders(response.serviceOrders);
+        const token = localStorage.getItem('authToken');
+        if (token) {
+          setAuthToken(token);
+          const response = await getOrdersByClientId(id);
+          setOrders(response.serviceOrders);
+          console.log('Órdenes obtenidas:', response.serviceOrders);
+        } else {
+          console.error('No hay token de autenticación disponible.');
+        }
       } catch (error) {
         console.error('Error al obtener las órdenes de servicio:', error);
       }
@@ -20,23 +27,33 @@ const OrderListClient = ({ id }) => {
     fetchOrders();
   }, [id]);
 
+  const handleViewClick = (orderId) => {
+    console.log('Order ID clicked:', orderId);
+    navigate(`/order-detail/${orderId}`);
+  };
+
   return (
     <div>
-      <h3>Órdenes de Servicio</h3>
-      {orders.length === 0 ? (
-        <p>No hay órdenes de servicio para este cliente.</p>
-      ) : (
-        <ul>
-          {orders.map(order => (
-            <li key={order.id}>
-              <p><strong>ID de Orden:</strong> {order.id}</p>
-              <p><strong>Descripción:</strong> {order.description}</p>
-              <p><strong>Fecha:</strong> {order.date}</p>
-              {/* Agrega más detalles de la orden según sea necesario */}
-            </li>
-          ))}
-        </ul>
-      )}
+      <h2>Lista de Órdenes de Servicio</h2>
+      <ListGroup>
+        {orders.length > 0 ? (
+          orders.map(order => (
+            <ListGroup.Item
+              key={order.serviceOrderId}
+              className="d-flex justify-content-between align-items-center"
+            >
+              <div>
+                {order.incidentDescription} - {order.statusServiceOrder} - {order.incidentCost}$
+              </div>
+              <Button variant="primary" onClick={() => handleViewClick(order.serviceOrderId)}>
+                Ver
+              </Button>
+            </ListGroup.Item>
+          ))
+        ) : (
+          <p>No hay órdenes disponibles para este vehículo.</p>
+        )}
+      </ListGroup>
     </div>
   );
 };
--- gruasucab-webclient/src/components/Vehicle/vehicleDetail.js
+++ gruasucab-webclient/src/components/Vehicle/vehicleDetail.js
@@ -5,20 +5,33 @@ import { getVehicleById, updateVehicle } from '../../api/apiVehicle'; // Añade 
 import { getOrdersByVehicleId } from '../../api/apiServiceOrder';
 import { getAllDrivers } from '../../api/apiUser';
 import { Container, Row, Col } from 'react-bootstrap'; // Importa Container, Row y Col
+import { setAuthToken } from '../../api/apiAuth'; // Importa setAuthToken
+import { getSupplierById } from '../../api/apiSuplier';
 
 const VehicleDetail = () => {
   const { id } = useParams();
   const [vehicle, setVehicle] = useState(null);
   const [orders, setOrders] = useState([]);
   const [drivers, setDrivers] = useState([]);
-  const [selectedDriver, setSelectedDriver] = useState('');
+  const [selectedDriver, setSelectedDriver] = useState(null);
+  const [error, setError] = useState(null);
+  const [supplier, setSupplier] = useState(null);
 
   useEffect(() => {
     const fetchVehicleData = async () => {
       try {
+        const token = localStorage.getItem('authToken');
+        if (token) {
+          setAuthToken(token);
+        } else {
+          throw new Error('No se encontró el token de autenticación');
+        }
         const vehicleData = await getVehicleById(id);
         setVehicle(vehicleData.vehicle);
-        setSelectedDriver(vehicleData.vehicle.driverId || '');
+        const driver = drivers.find(driver => driver.id === vehicleData.vehicle.driverId);
+        setSelectedDriver(driver || null);
+        const supplierData = await getSupplierById(vehicleData.vehicle.supplierId);
+        setSupplier(supplierData.supplier);
         console.log("Vehiculo:", vehicleData.vehicle);
       } catch (error) {
         console.error('Error al cargar los datos:', error);
@@ -29,7 +42,7 @@ const VehicleDetail = () => {
     if (id) {
       fetchVehicleData();
     }
-  }, [id]);
+  }, [id, drivers]);
 
   useEffect(() => {
     const fetchAllOrders = async () => {
@@ -66,15 +79,16 @@ const VehicleDetail = () => {
   }
 
   const handleDriverChange = async (e) => {
-    const newDriverId = e.target.value;
-    if (newDriverId) {
-      setSelectedDriver(newDriverId);
+    const driverId = e.target.value;
+    const driver = drivers.find(driver => driver.id === driverId);
+    if (driver) {
+      setSelectedDriver(driver);
       try {
         const updatedVehicle = {
           userEmail: localStorage.getItem('userEmail'),
           userId: localStorage.getItem('userID'),
           vehicleId: vehicle.vehicleId,
-          driverId: newDriverId,
+          driverId: driver.id,
           supplierId: vehicle.supplierId,
           civilLiability: vehicle.civilLiability,
           civilLiabilityExpirationDate: vehicle.civilLiabilityExpirationDate,
@@ -86,12 +100,14 @@ const VehicleDetail = () => {
           vehicleTypeId: vehicle.vehicleTypeId
         }; // Actualiza el vehículo con el nuevo conductor
         await updateVehicle(updatedVehicle); // Llama a updateVehicle con el vehículo actualizado
+        alert('Vehículo actualizado con éxito');
       } catch (error) {
         console.error('Error al actualizar el conductor:', error);
-        alert('Error al actualizar el conductor');
+        setError('Error al actualizar el conductor. Verifique sus permisos.');
+        alert('Error al actualizar el conductor. Verifique sus permisos.');
       }
     } else {
-      setSelectedDriver('');
+      setSelectedDriver(null);
       console.log("Conductor no asignado");
     }
   };
@@ -99,21 +115,22 @@ const VehicleDetail = () => {
   return (
     <Container>
       <h2>Detalles del Vehiculo</h2>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
       <Row className="mb-4">
         <Col>
           <p><strong>Marca:</strong> {vehicle.brand}</p>
           <p><strong>Modelo:</strong> {vehicle.model}</p>
           <p><strong>Color:</strong> {vehicle.color}</p>
           <p><strong>Placa:</strong> {vehicle.licensePlate}</p>
-          <p><strong>Proveedor:</strong> {vehicle.supplierId}</p>
-          <p><strong>Conductor:</strong> {selectedDriver || 'Conductor no asignado'}</p>
+          <p><strong>Proveedor:</strong> {supplier ? supplier.name : 'Cargando...'}</p>
+          <p><strong>Conductor:</strong> {selectedDriver ? selectedDriver.name : 'Conductor no asignado'}</p>
           <select
-            value={selectedDriver}
+            value={selectedDriver ? selectedDriver.id : ''}
             onChange={handleDriverChange}
             className="input-field"
             style={{ width: "300px" }}
           >
-            <option value="" disabled={selectedDriver === ''}>Asignar Conductor</option>
+            <option value="" disabled={selectedDriver === null}>Asignar Conductor</option>
             {drivers.map(driver => (
               <option key={driver.id} value={driver.id}>
                 {driver.name}
